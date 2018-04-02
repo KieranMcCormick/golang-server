e59159b2c60604506facbba190c446c35ecd6d67
liumingl <liumingl@sfu.ca>
2018-04-02 10:18:35 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+
+if [ ! -d "go" ]; then
+    wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+fi
 tar -xzvf go1.10.1.linux-amd64.tar.gz
-rm go1.10.linux-amd64.tar.gz
+rm go1.10.1.linux-amd64.tar.gz
 
 # go build server.go config.go request.go fileHandler.go logging.go
 ./go/bin/go build server.go config.go request.go fileHandler.go logging.go
